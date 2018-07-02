a6ca4618fef6b31df912aa89ea9af6cb5ae2c52b
Adam <adamgru@onet.pl>
2018-07-02 17:40:13 +0200

    some changes

--- src/Hello.java
+++ src/Hello.java
@@ -1,12 +1,32 @@
-public class Hello {
+
+public class Hello implements Phone {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello World");
+
+        Hello hello = new Hello();
+        hello.call();
+        hello.sms();
+    }
+    @Override
+    public void call() {
+        System.out.println("Phone is calling");
+    }
+
+    @Override
+    public void sms() {
+        System.out.println("SMS is sending");
     }
 }
 
+interface Phone
+{
+    void call();
+    void sms();
+}
 
 // in git ignore
 //.idea target out
 //pwd
+//git checkout
+//git pull
+//git merge master
