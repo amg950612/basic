e88527d05ffde8ca933e06a25043b22b070f9c13
Adam <adamgru@onet.pl>
2018-07-12 11:55:41 +0200

    app

--- src/ListEx.java
+++ src/ListEx.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ListEx {
+public class ListEx  {
+
+    public static void main(String[] args)  {
+
+        List<String> names = new ArrayList<>();
+        names.add("Kris");
+        names.add("Ira");
+
+        System.out.println("Size = " + names.size());
+
+        for(String x:names)
+        {
+            System.out.println(x);
+        }
+
+        System.out.println( names.contains("Adam") );
+    }
 }
